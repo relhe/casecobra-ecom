05bb0125f67ffb5b3221b11ccac41f6fff4cda30
Renel Lherisson <renellherisson@gmail.com>
2024-10-06 23:24:01 -0400

    RL |conf| image can display correctly with correct path from public directory

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,4 @@
+import { Check } from "lucide-react";
 import MaxWidthWrapper from "../components/MaxWidthWrapper";
 import Image from "next/image";
 
@@ -9,12 +10,7 @@ export default function Home() {
           <div className="col-span-2 px-6 lg:px-0 lg:pt-4">
             <div className="relative mx-auto text-center lg:text-left flex flex-col items-center lg:items-start">
               <div className="absolute w-28 left-0 -top-20 hidden lg:block">
-                <Image
-                  src="/public/snake-1.png"
-                  alt="bob"
-                  width={800}
-                  height={500}
-                />
+                <Image src="/snake-1.png" alt="bob" width={800} height={500} />
               </div>
               <h1 className="relative w-fit tracking-tight text-balance mt-16 font-bold !leading-tight text-gray-900 text-5xl md:text-6xl lg:text-7xl">
                 Your image on a{" "}
@@ -28,6 +24,31 @@ export default function Home() {
                 CaseCobra allows you to protect your memories, not just your
                 phone case.
               </p>
+
+              <ul className="mt-8 space-y-2 text-left font-medium flex flex-col items-center sm:items-start">
+                <li className="flex gap-1.5 items-center text-left">
+                  <Check className="h-5 w-5 shrink-0 text-green-600" />
+                  High quality, durable material
+                </li>
+                <li className="flex gap-1.5 items-center text-left">
+                  <Check className="h-5 w-5 shrink-0 text-green-600" />5 year
+                  print guarantee
+                </li>
+                <li className="flex gap-1.5 items-center text-left">
+                  <Check className="h-5 w-5 shrink-0 text-green-600" />
+                  Modern iphone model supported
+                </li>
+              </ul>
+              <div className="mt-12 flex flex-col sm:flex-row items-center sm:items-start gap-5"></div>
+              <div className="flex -space-x-4">
+                <Image
+                  className="inline-block round-full ring-2 ring-slate-100"
+                  src="/users/user6.jpg"
+                  alt={"user"}
+                  width={40}
+                  height={40}
+                />
+              </div>
             </div>
           </div>
         </MaxWidthWrapper>
