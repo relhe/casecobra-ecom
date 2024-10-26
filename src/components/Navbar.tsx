4a8a4bfa461c6009a01089262081dd7cd2427e3b
relhe <renel.lherisson@polymtl.ca>
2024-10-26 17:46:29 -0400

    RL | fix | fix justify in header

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -9,11 +9,11 @@ const Navbar = () => {
     return (
         <nav className="sticky z-[100] h-14 insert-x-0 top-0 w-full border-b border-gray-200 bg-white/75 backdrop-blur-lg translate-all">
             <MaxWidthWrapper>
-                <div className="flex h-14 items-center justify-center border-b border-zinc-200">
+                <div className="flex h-14 items-center justify-between border-b border-zinc-200">
                     <Link href="/" className="flex z-40 font-semibold">
                         case <span className="text-green-600">cobra</span>
                     </Link>
-                    <div className="flex items-center space-x-4 h-full">
+                    <div className=" h-full flex items-center space-x-4">
                         {user ? (
                             <>
                                 <Link
