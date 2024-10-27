6e8e9f73abe50461c29b49758fed18db03d43ad6
Renel Lherisson <renellherisson@gmail.com>
2024-10-27 18:37:56 -0400

    RL | feature | added new icon file

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,7 @@ import { Check, Star } from 'lucide-react';
 import MaxWidthWrapper from '../components/MaxWidthWrapper';
 import Image from 'next/image';
 import Phone from '@/components/Phone';
+import { Icons } from '@/components/Icons';
 
 export default function Home() {
     return (
@@ -143,6 +144,29 @@ export default function Home() {
                                 imgSrc="/testimonials/1.jpg"
                             />
                         </div>
+                    </div>
+                </MaxWidthWrapper>
+            </section>
+            <section className="bg-slate-100 py-24">
+                {/* Value proposition section */}
+                <MaxWidthWrapper className="flex flex-col items-center gap-16 sm:gap-32">
+                    <div className="flex flex-col lg:flex-row items-center gap-4 sm:gap-6">
+                        <h2 className="order-1 mt-2 tracking-tight text-center text-balance !leading-tight font-bold text-5xl md:text-6xl text-gray-900">
+                            What our{' '}
+                            <span className="relative px-2">
+                                {' '}
+                                customers
+                                <Icons.underline className="hidden sm:block pointer-events-none absolute inset-x-0 -bottom-6 text-green-500" />
+                            </span>{' '}
+                            say
+                        </h2>
+                        <Image
+                            className="order-0 lg:order-2"
+                            src="/snake-2.png"
+                            alt="snake image"
+                            width={100}
+                            height={80}
+                        />
                     </div>
                 </MaxWidthWrapper>
             </section>
